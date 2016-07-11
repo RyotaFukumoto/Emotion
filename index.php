a695908584ae46cbec639392943cc88cb8f83451
Hayashi <k014c1105@it-neec.jp>
2016-07-11 10:23:26 +0900

    aaaaaaaa

--- index.php
+++ index.php
@@ -1,33 +1,46 @@
+<?php
+	if(isset($_GET['submit'])){
+
+	}
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
-<script type="text/javascript" src="../canvasjs/canvasjs.min.js"></script>
-<script type="text/javascript">
-
-window.onload = function () {
-	var chart = new CanvasJS.Chart("chartContainer", {
-		theme: "theme2",//theme1
-		title:{
-			text: "Sample Chart"
-		},
-		animationEnabled: false,   // change to true
-		data: [
-		{
-			// Change type to "bar", "area", "spline", "pie",etc.
-			type: "column",
-			dataPoints: [
-				{ label: "like,dislike",  y: -25  },
-				{ label: "orange", y: 15  },
-				{ label: "banana", y: 30000  },
-			]
+	<meta charset="utf-8">
+	<script type="text/javascript" src="../canvasjs/canvasjs.min.js"></script>
+	<script type="text/javascript">
+		window.onload = function () {
+			var chart = new CanvasJS.Chart("chartContainer", {
+				theme: "theme2",//theme1
+				title:{
+					text: "result"
+				},
+				animationEnabled: false,   // change to true
+				data: [
+				{
+					// Change type to "bar", "area", "spline", "pie",etc.
+					type: "column",
+					dataPoints: [
+						{ label: "like,dislike",  y: -25  },
+						{ label: "joy,sad", y: 15  },
+						{ label: "anger,fear", y: 10  },
+					]
+				}
+				]
+			});
+			chart.render();
 		}
-		]
-	});
-	chart.render();
-}
-</script>
+	</script>
 </head>
 <body>
-<div id="chartContainer" style="height: 300px; width: 100%;"></div>
+	<div style="text-align:center;">
+		<form action=""　method="get">
+			<input type="text" name="text"　placeholder="テキストを入力してください">
+			<button type="submit" name="submit"　value="">検索</button>
+		</form>
+	</div>
+	<div style="text-align:center;">
+		<div id="chartContainer" style="height: 250px; width: 80%;margin:auto"></div>
+	</div>
 </body>
 </html>
