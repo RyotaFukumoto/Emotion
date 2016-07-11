8ea7aee1456d5c941def8bcff67674499525982b
Hayashi <k014c1105@it-neec.jp>
2016-07-11 09:15:40 +0900

    farst commit

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<script type="text/javascript" src="../canvasjs/canvasjs.min.js"></script>
+<script type="text/javascript">
 
+window.onload = function () {
+	var chart = new CanvasJS.Chart("chartContainer", {
+		theme: "theme2",//theme1
+		title:{
+			text: "Sample Chart"
+		},
+		animationEnabled: false,   // change to true
+		data: [
+		{
+			// Change type to "bar", "area", "spline", "pie",etc.
+			type: "column",
+			dataPoints: [
+				{ label: "like,dislike",  y: -25  },
+				{ label: "orange", y: 15  },
+				{ label: "banana", y: 30000  },
+			]
+		}
+		]
+	});
+	chart.render();
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 300px; width: 100%;"></div>
+</body>
+</html>
